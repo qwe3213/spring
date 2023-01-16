65f19ab30aba43654d35b11beb5dc0cfd01d9e46
hayun <hs183000@naver.com>
2023-01-16 23:27:42 +0900

    MVC와 템플릿 엔진

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //spring은 해주어야함
 @Controller
@@ -12,4 +13,10 @@ public class HelloController {
         model.addAttribute("data" , "spring ");
         return "hello"; // resources/templates/hello 파일을 찾아서 렌더링함.
     }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name" ,name);
+        return "hello-template";
+    }
 }
+//heelo-mvc? name=이름칠것 <-이런식으로 하여야 페이지에 오류가 뜨지 않고 제대로 나옴
