b7406cd1b122c1216c362c9ae98e560679c835b9
hayun <hs183000@naver.com>
2023-01-24 19:53:44 +0900

    돌때마다 DB값 날려주기

--- hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -1,7 +1,9 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,13 +11,18 @@ import static org.assertj.core.api.Assertions.*;
 class MemberServiceTest {
 
        MemberService memberService = new MemberService();
+      MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+    @AfterEach
+    public void afterEach(){
+        memberRepository.clearStore();
+    } // 돌떄마다 DB의 값을 날려줌
 
     @Test
     void 회원가입() {
         //테스트 같은경우 한글로 써도 무방함.
         //given
         Member member = new Member();
-        member.setName("Hello");
+        member.setName("spring");
        //when
         Long saveId = memberService.join(member);
 
