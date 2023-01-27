c24b87142ecee601659721930803000df42649ad
hayun <hs183000@naver.com>
2023-01-27 23:33:17 +0900

    자바코드로 스프링 빈 등록하기

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -8,10 +8,10 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MemberController {
    private final MemberService memberService;
-
+//   @Autowired private final MemberService memberService; //           필드주입식 방법
     @Autowired
     public MemberController(MemberService memberService){
         this.memberService = memberService;
     }
-    //   @Autowired는 스프링 컨테이너에서 memberService을 가져오는것이다.
+    //   @Autowired는 스프링 컨테이너에서 memberService을 가져오는것이다. 생성자 주입식 , 대부분 생성자 주입식사용하고 권장함
 }
